330a468c91ad1e7f1da2aa06a8fed6172a1bd2ee
Pasindu W <117517146+Pasindu0214@users.noreply.github.com>
2024-07-15 23:20:40 +0530

    Create Honeypot.py

---
+++ Honeypot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import socket
+
+def main():
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock.bind(('', 2222))
+    server_sock.listen(127)
+
+if __name__ == "__main__":
+    main()
